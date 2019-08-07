9aa86a51866110d99e9a18075f4dedeff24a6766
Sonia MAC <sonia@gmail.com>
2019-08-06 17:27:50 -0700

    initial commit

---
+++ tabletest/tabletest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabletest
+//
+//  Created by piscos on 06/08/2019.
+//  Copyright © 2019 piscos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
